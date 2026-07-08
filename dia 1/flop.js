284795f5eed38aee49b5f4622afe5c2647f3df84
k4abrego <188397826+k4abrego@users.noreply.github.com>
2026-07-08 12:58:18 -0600

    Add initial HTML and JavaScript files, including content for 'textos.html', 'imagenes 2.html', and 'flop.js'; create new text files and update .DS_Store

---
+++ dia 1/flop.js
@@ -0,0 +1,50 @@
+
+
+let resultado = 5 + 3;
+
+
+
+
+let puntos = 10 - 4;
+
+
+
+
+
+
+let total = 4 * 3;
+
+
+
+
+
+let puntos = 10;
+puntos = puntos + 5;
+
+
+
+
+10 >= 10
+
+
+
+let mensaje = "Hola";
+mensaje = mensaje + " bestie";
+
+
+
+let antojo = "Quiero";
+antojo = antojo + " tacos";
+antojo = antojo + " y una coquita";
+
+
+
+
+let tieneTarea = true;
+let tieneInternet = true;
+
+tieneTarea && tieneInternet
+
+
+
+
