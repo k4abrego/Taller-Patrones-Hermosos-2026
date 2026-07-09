4873666c1da3e9df0652709e0460ddc273e90a80
k4abrego <188397826+k4abrego@users.noreply.github.com>
2026-07-08 22:46:16 -0600

    Add conditional logic examples to flop.js and create FLOP.TXT for practice

--- dia 1/flop.js
+++ dia 1/flop.js
@@ -48,3 +48,86 @@ tieneTarea && tieneInternet
 
 
 
+if (condición) {
+  // pasa algo
+} else {
+  // pasa otra cosa
+}
+
+
+
+let edad = 20;
+
+if (edad ___ 18) {
+  alert("Puedes entrar");
+} else {
+  alert("No puedes entrar");
+}
+
+
+let puntos = 120;
+
+if (puntos >= 100) {
+  alert("¡Ganaste el nivel!");
+} else {
+  alert("Sigue intentando");
+}
+
+
+
+
+let puntos = 80;
+
+if (puntos >= 100) {
+  alert("¡Ganaste el nivel!");
+} else {
+  alert("Sigue intentando");
+}
+
+
+
+
+let tieneEfectivo = false;
+let tieneTarjeta = true;
+
+if (tieneEfectivo ___ tieneTarjeta) {
+  alert("Puedes pagar");
+} else {
+  alert("No puedes pagar");
+}
+
+
+
+
+
+let tieneEfectivo = false;
+let tieneTarjeta = true;
+
+if (tieneEfectivo || tieneTarjeta) {
+  alert("Puedes pagar");
+} else {
+  alert("No puedes pagar");
+}
+
+
+
+let tieneBoleto = true;
+let tienePermiso = true;
+
+if (tieneBoleto ___ tienePermiso) {
+  alert("Puedes entrar a la fiesta");
+} else {
+  alert("No puedes entrar");
+}
+
+
+let tieneBoleto = true;
+let tienePermiso = false;
+
+if (tieneBoleto && tienePermiso) {
+  alert("Puedes entrar a la fiesta");
+} else {
+  alert("No puedes entrar");
+}
+
+
